96be68075c2b201dc8fbf81e999608d15e9d4ebf
Janet Yu <janety837@gmail.com>
2024-04-04 14:33:58 -0400

    send

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		<input type="checkbox" id="my_modal_7" class="modal-toggle" />
 		<div class="modal" role="dialog">
 			<div class="modal-box">
-				<h3 class="text-lg font-bold">Create post</h3>
+				<h3 class="text-lg font-bold">Create post!</h3>
 			</div>
 			<label class="modal-backdrop" for="my_modal_7">Close</label>
 		</div>
